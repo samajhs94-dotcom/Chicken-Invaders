f5a907a6c93de008fdc0499c311fd896848c850a
Sama <samajhs94@gmail.com>
2026-06-29 20:25:56 +0330

    feat: implement main menu

---
+++ src/ui/MainMenu.java
@@ -0,0 +1,154 @@
+package ui;
+
+import main.GameMain;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MainMenu extends BackgroundPanel{
+
+
+    private GameMain frame;
+    private JButton newGameButton;
+    private JButton highScoreButton;
+    private JButton settingsButton;
+    private JButton howToPlayButton;
+    private JButton exitButton;
+    private JLabel messageLabel;
+
+    public MainMenu(GameMain frame){
+        super("src/resources/images/welcomeBackground.png");
+        this.frame=frame;
+        initializeComponents();
+        addListeners();
+    }
+
+    private void initializeComponents() {
+
+        setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(12,12,12,12);
+
+        JLabel title = new JLabel("CHICKEN INVADERS");
+        title.setFont(new Font("Arial", Font.BOLD, 36));
+        title.setForeground(Color.WHITE);
+
+        messageLabel = new JLabel(" ");
+        messageLabel.setForeground(Color.RED);
+        messageLabel.setFont(new Font("Arial", Font.BOLD, 14));
+        messageLabel.setHorizontalAlignment(SwingConstants.CENTER);
+
+        newGameButton = new JButton("New Game");
+        highScoreButton = new JButton("High Scores");
+        settingsButton = new JButton("Settings");
+        howToPlayButton = new JButton("How To Play");
+        exitButton = new JButton("Exit");
+
+        Dimension buttonSize = new Dimension(220,45);
+
+        newGameButton.setPreferredSize(buttonSize);
+        highScoreButton.setPreferredSize(buttonSize);
+        settingsButton.setPreferredSize(buttonSize);
+        howToPlayButton.setPreferredSize(buttonSize);
+        exitButton.setPreferredSize(buttonSize);
+
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+
+        add(title,gbc);
+
+        gbc.gridy++;
+        add(messageLabel, gbc);
+
+        gbc.gridy++;
+        add(newGameButton,gbc);
+
+        gbc.gridy++;
+        add(highScoreButton,gbc);
+
+        gbc.gridy++;
+        add(settingsButton,gbc);
+
+        gbc.gridy++;
+        add(howToPlayButton,gbc);
+
+        gbc.gridy++;
+        add(exitButton,gbc);
+    }
+
+
+    private void addListeners() {
+
+        // New Game
+        newGameButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                if (frame.getCurrentUser() == null) {
+                    frame.showLoginPanel();
+                }
+                else {
+                    JOptionPane.showMessageDialog(MainMenu.this, "Start Game");
+                    frame.showGamePanel();
+                }
+            }
+        });
+
+        // High Scores
+        highScoreButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                JOptionPane.showMessageDialog(MainMenu.this, "High Scores Panel");
+                frame.showHighScorePanel();
+            }
+        });
+
+        // Settings
+        settingsButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                if(frame.getCurrentUser() == null){
+                    messageLabel.setText("Please login first!");
+
+                    // پاک شدن پیام بعد از ۳ ثانیه
+                    Timer timer = new Timer(3000, new ActionListener() {
+                        @Override
+                        public void actionPerformed(ActionEvent e) {
+                            messageLabel.setText(" ");
+                        }
+                    });
+
+                    timer.setRepeats(false);
+                    timer.start();
+                    return;
+                }
+                frame.showSettingsPanel();
+
+            }
+        });
+
+        // How To Play
+        howToPlayButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                JOptionPane.showMessageDialog(MainMenu.this, "How To Play Panel");
+                frame.showHowToPlayPanel();
+            }
+        });
+
+        // Exit
+        exitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+    }
+
+
+}
