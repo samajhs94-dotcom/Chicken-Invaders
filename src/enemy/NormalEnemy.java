4906dcd1b37808d4e457b5b9ce35d6baa2b29d9b
Sama <samajhs94@gmail.com>
2026-07-01 14:55:25 +0330

    Implement NormalEnemy

--- src/enemy/NormalEnemy.java
+++ src/enemy/NormalEnemy.java
@@ -1,4 +1,15 @@
 package enemy;
 
-public class NormalEnemy {
+import javax.swing.*;
+
+public class NormalEnemy extends Enemy{
+
+    public NormalEnemy(int x, int y, int health){
+        super(x, y, 1, health, new ImageIcon("src/resources/images/NormalEnemy.png").getImage());
+    }
+
+    @Override
+    public void move(){}
+    //چون حرکت مرغ ها شبکه ایه و تو بخش دیگه ای کنترل میشه
+
 }
