9fd0cf6bcc231c793586865446fc0a42d09ed21e
Sama <samajhs94@gmail.com>
2026-07-03 18:33:04 +0330

    feat(enemy): implement enemy base and derived classes

--- src/enemy/Enemy.java
+++ src/enemy/Enemy.java
@@ -70,5 +70,11 @@ public abstract class Enemy {
         return height;
     }
 
+    public void setX(int x) {
+        this.x = x;
+    }
 
+    public void setY(int y) {
+        this.y = y;
+    }
 }
--- src/enemy/ShooterEnemy.java
+++ src/enemy/ShooterEnemy.java
@@ -1,4 +1,29 @@
 package enemy;
 
-public class ShooterEnemy {
+import javax.swing.*;
+
+public class ShooterEnemy extends Enemy {
+
+    private long lastShotTime;
+
+    public ShooterEnemy(int x,int y){
+        super(x, y, 1.0, 3, new ImageIcon("src/resources/images/shooterChicken.png").getImage());
+    }
+
+    @Override
+    public void move(){
+        // حرکت شبکه توسط GamePanel
+    }
+
+    public boolean canShoot(){
+
+        long current=System.currentTimeMillis();
+        if(current-lastShotTime>=2000){
+            lastShotTime=current;
+            return true;
+        }
+        return false;
+
+    }
+
 }
