4b5863387bdc228c3753562d75c35c40bf51ddb4
Sama <samajhs94@gmail.com>
2026-07-08 00:15:34 +0330

    Finalize database manager and improve cell behavior+Debugging

--- src/manager/DatabaseManager.java
+++ src/manager/DatabaseManager.java
@@ -14,8 +14,7 @@ public class DatabaseManager {
             System.out.println("Connected to database successfully.");
             return true;
         } catch (SQLException e) {
-            System.out.println("Failed to connect to database.");
-            e.printStackTrace();
+            System.out.println("Failed to connect to database." + e.getMessage());
             return false;
         }
     }
@@ -26,6 +25,8 @@ public class DatabaseManager {
                 + "id INTEGER PRIMARY KEY AUTOINCREMENT, "
                 + "username TEXT UNIQUE NOT NULL, "
                 + "password TEXT NOT NULL, "
+                + "high_score INTEGER DEFAULT 0, "
+                + "last_level INTEGER DEFAULT 1, "
                 + "music_enabled BOOLEAN DEFAULT TRUE, "
                 + "shot_sound_enabled BOOLEAN DEFAULT TRUE, "
                 + "explosion_sound_enabled BOOLEAN DEFAULT TRUE, "
@@ -38,6 +39,10 @@ public class DatabaseManager {
                 + "score INTEGER NOT NULL, "
                 + "level INTEGER NOT NULL, "
                 + "game_date DATETIME DEFAULT CURRENT_TIMESTAMP, "
+                + "music_enabled BOOLEAN DEFAULT TRUE, "
+                + "shot_sound_enabled BOOLEAN DEFAULT TRUE, "
+                + "explosion_sound_enabled BOOLEAN DEFAULT TRUE, "
+                + "game_over_sound_enabled BOOLEAN DEFAULT TRUE, "
                 + "FOREIGN KEY(user_id) REFERENCES users(id)"
                 + ");";
 
@@ -57,10 +62,9 @@ public class DatabaseManager {
 
     public void registerUser(User user) throws SQLException {
 
-        String sql = "INSERT INTO users(username,password,music_enabled,"
-                + "shot_sound_enabled,explosion_sound_enabled,"
-                + "game_over_sound_enabled)"
-                + " VALUES(?,?,?,?,?,?)";
+        String sql = "INSERT INTO users(username,password,high_score,last_level,music_enabled,"
+                + "shot_sound_enabled,explosion_sound_enabled,game_over_sound_enabled)"
+                + " VALUES(?,?,?,?,?,?,?,?)";
 
         String username = user.getUsername();
 
@@ -76,10 +80,12 @@ public class DatabaseManager {
 
         ps.setString(1, user.getUsername());
         ps.setString(2, user.getPassword());
-        ps.setBoolean(3, user.getMusicEnabled());
-        ps.setBoolean(4, user.getShotSoundEnabled());
-        ps.setBoolean(5, user.getExplosionSoundEnabled());
-        ps.setBoolean(6, user.getGameOverSoundEnabled());
+        ps.setInt(3, 0);
+        ps.setInt(4, 1);
+        ps.setBoolean(5, user.getMusicEnabled());
+        ps.setBoolean(6, user.getShotSoundEnabled());
+        ps.setBoolean(7, user.getExplosionSoundEnabled());
+        ps.setBoolean(8, user.getGameOverSoundEnabled());
 
         ps.executeUpdate();
 
@@ -91,6 +97,9 @@ public class DatabaseManager {
 
         rs.close();
         ps.close();
+
+        user.setHighScore(0);
+        user.setLastLevel(1);
     }
 
     public User login(String username, String password) throws SQLException {
@@ -107,11 +116,12 @@ public class DatabaseManager {
 
             User user = new User(rs.getString("username"), rs.getString("password"));
             user.setId(rs.getInt("id"));
+            user.setHighScore(rs.getInt("high_score"));
+            user.setLastLevel(rs.getInt("last_level"));
             user.setMusicEnabled(rs.getBoolean("music_enabled"));
             user.setShotSoundEnabled(rs.getBoolean("shot_sound_enabled"));
             user.setExplosionSoundEnabled(rs.getBoolean("explosion_sound_enabled"));
             user.setGameOverSoundEnabled(rs.getBoolean("game_over_sound_enabled"));
-            loadGameStats(user);
             rs.close();
             prstm.close();
             return user;
@@ -122,46 +132,129 @@ public class DatabaseManager {
     }
 
 
-    //متد کمکی لاگین
-    private void loadGameStats(User user) throws SQLException {
-
-        String sqr= "SELECT MAX(score) FROM game_history WHERE user_id=?";
-        String sqr2= "SELECT Max(level) FROM game_history WHERE user_id=?" ;
+    public void saveGame(User user, int score, int level, boolean musicEnabled, boolean shotSoundEnabled,
+                         boolean explosionSoundEnabled, boolean gameOverSoundEnabled) {
 
 
-        // High Score
-        PreparedStatement ps1 = connection.prepareStatement(sqr);
-        ps1.setInt(1,user.getId());
-        ResultSet rs1 = ps1.executeQuery();
-        user.setHighScore( rs1.next() ? rs1.getInt(1) : 0);
-        rs1.close();
-        ps1.close();
-
-        // Last Level
-        PreparedStatement ps2 = connection.prepareStatement(sqr2);
-        ps2.setInt(1, user.getId());
-        ResultSet rs2 = ps2.executeQuery();
-        user.setLastLevel(rs2.next() ? rs2.getInt(1) : 1);
-        rs2.close();
-        ps2.close();
-
-
-    }
-
-    public void saveGame(User user, int score, int level) {
+        if (user == null) {
+            return;
+        }
 
         try {
+            //اپدیت خلاصه وضعیت کاربر و ذخیره رکورد کامل بازی
+            connection.setAutoCommit(false);
+
             //ذخیره در هیستوری
-            String sql = "INSERT INTO game_history(user_id, score, level) VALUES (?, ?, ?)";
+            String sql = "INSERT INTO game_history(user_id, score, level ,music_enabled,"
+            + " shot_sound_enabled, explosion_sound_enabled, game_over_sound_enabled)" +
+                    " VALUES (?, ?, ?, ?, ?, ? , ? )";
             PreparedStatement ps = connection.prepareStatement(sql);
             ps.setInt(1, user.getId());
             ps.setInt(2, score);
             ps.setInt(3, level);
+            ps.setBoolean(4, musicEnabled);
+            ps.setBoolean(5, shotSoundEnabled);
+            ps.setBoolean(6, explosionSoundEnabled);
+            ps.setBoolean(7, gameOverSoundEnabled);
+            ps.executeUpdate();
+            ps.close();
+
+            //اپدیت خلاصه وضعیت کاربر
+            String updateUserSql = "UPDATE users SET "
+                    + "high_score = CASE WHEN ? > high_score THEN ? ELSE high_score END, "
+                    + "last_level = ?, "
+                    + "music_enabled = ?, "
+                    + "shot_sound_enabled = ?, "
+                    + "explosion_sound_enabled = ?, "
+                    + "game_over_sound_enabled = ? "
+                    + "WHERE id = ?";
+
+            PreparedStatement updatePs = connection.prepareStatement(updateUserSql);
+
+            //اگر امتیاز جدیدی بیشتر از قبلی باشه اپدیت میشه
+            updatePs.setInt(1, score);
+            updatePs.setInt(2, score);
+
+            updatePs.setInt(3, level);
+            updatePs.setBoolean(4, musicEnabled);
+            updatePs.setBoolean(5, shotSoundEnabled);
+            updatePs.setBoolean(6, explosionSoundEnabled);
+            updatePs.setBoolean(7, gameOverSoundEnabled);
+            updatePs.setInt(8, user.getId());
+
+            updatePs.executeUpdate();
+            updatePs.close();
+
+            // اگر همه چیز موفق بود، تغییرات در دیتابیس ثبت نهایی می‌شود
+            connection.commit();
+
+            //  اتصال دیتابیس را به حالت عادی برمی‌گردانی
+            connection.setAutoCommit(true);
+
+            //اپدیت ابجکت یوزر همراه با دیتابیس
+            if (score > user.getHighScore()) {
+                user.setHighScore(score);
+            }
+            user.setLastLevel(level);
+
+            user.setMusicEnabled(musicEnabled);
+            user.setShotSoundEnabled(shotSoundEnabled);
+            user.setExplosionSoundEnabled(explosionSoundEnabled);
+            user.setGameOverSoundEnabled(gameOverSoundEnabled);
+
+        }
+        catch (SQLException e) {
+            // نوت: اگر خطا رخ داد، تغییرات نصفه‌نیمه برگردانده می‌شود
+            try {
+                connection.rollback();
+                connection.setAutoCommit(true);
+            } catch (SQLException rollbackException) {
+                System.out.println("Rollback error: " + rollbackException.getMessage());
+            }
+            System.out.println("Save game error: " + e.getMessage());
+        }
+
+    }
+
+
+    public void updateSoundSettings(User user) {
+
+        String sql = "UPDATE users SET "
+                + "music_enabled=?, "
+                + "shot_sound_enabled=?, "
+                + "explosion_sound_enabled=?, "
+                + "game_over_sound_enabled=? "
+                + "WHERE id=?";
+
+        try {
+            PreparedStatement ps = connection.prepareStatement(sql);
+
+            ps.setBoolean(1, user.getMusicEnabled());
+            ps.setBoolean(2, user.getShotSoundEnabled());
+            ps.setBoolean(3, user.getExplosionSoundEnabled());
+            ps.setBoolean(4, user.getGameOverSoundEnabled());
+            ps.setInt(5, user.getId());
+
             ps.executeUpdate();
             ps.close();
 
         } catch (SQLException e) {
+            System.out.println("Update sound settings error: " + e.getMessage());
+         }
+
+    }
+
+    //بستن اتصال دیتابیس هنگام خروج از برنامه
+    public void close() {
+
+        try {
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+            }
+
+        } catch (SQLException e) {
             System.out.println(e.getMessage());
+
         }
     }
 
